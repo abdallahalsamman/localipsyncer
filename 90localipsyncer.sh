9bc501217bdf8a6a9ad4bf86e77ab1efd7705f46
Your Name <you@example.com>
2024-03-02 17:20:25 +0200

    update

--- 90localipsyncer.sh
+++ 90localipsyncer.sh
@@ -7,10 +7,10 @@ if [ "$IF" == "wlp2s0" ]
 then
     case "$2" in
         up)
-		sudo -u user XDG_RUNTIME_DIR=/run/user/$(id -u user) /home/user/dev/localipsyncer/up.sh
+		/home/user/dev/localipsyncer/up.sh
         ;;
         down)
-		sudo -u user XDG_RUNTIME_DIR=/run/user/$(id -u user) /home/user/dev/localipsyncer/down.sh
+		/home/user/dev/localipsyncer/down.sh
         ;;
     esac
 fi
