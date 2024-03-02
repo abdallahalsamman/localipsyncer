8e952cfe626ff71b9dea1545bb6a6ecc2975e2d1
Your Name <you@example.com>
2024-03-02 17:40:15 +0200

    update

--- up.sh
+++ up.sh
@@ -1,7 +1,14 @@
 
 cd /home/user/dev/localipsyncer
 IP_ADDRESS=$(ip addr show wlp2s0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-echo "$IP_ADDRESSS<script>window.location.href = 'http://$IP_ADDRESS'</script>" > ./index.html
+echo "$IP_ADDRESSS
+<script>
+const port = 
+const urlParams = new URLSearchParams(window.location.search);
+const port = urlParams.get('port');
+window.location.href = 'http://$IP_ADDRESS:' + port
+</script>
+" > ./index.html
 git add .
 git commit -m "update"
 git push
