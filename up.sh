8cdc7165647d1b723e8c721438ca5241d602e05f
Your Name <you@example.com>
2024-03-02 17:46:30 +0200

    update

--- up.sh
+++ up.sh
@@ -3,7 +3,6 @@ cd /home/user/dev/localipsyncer
 IP_ADDRESS=$(ip addr show wlp2s0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 echo "$IP_ADDRESSS
 <script>
-const port = 
 const urlParams = new URLSearchParams(window.location.search);
 const port = urlParams.get('port');
 window.location.href = 'http://$IP_ADDRESS:' + port
