255e8941956e60ecc534704f5b796928ed6cf519
Your Name <you@example.com>
2024-03-02 17:31:01 +0200

    update

--- up.sh
+++ up.sh
@@ -1,6 +1,6 @@
 
 cd /home/user/dev/localipsyncer
 IP_ADDRESS=$(ip addr show wlp2s0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-echo "$IP_ADDRESSS<script>window.location.href = '$IP_ADDRESS'</script>" > ./index.html
+echo "$IP_ADDRESSS<script>window.location.href = 'http://$IP_ADDRESS'</script>" > ./index.html
 git commit -am "update"
 git push
